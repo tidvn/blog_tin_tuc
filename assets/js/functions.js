f6a8d25c73666f864ba907350cf98c5950243a98
tidvn <74198018+tidvn@users.noreply.github.com>
2021-11-13 18:03:51 +0700

    Update functions.js

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -13,20 +13,3 @@ const slider = tns({
 
  
   });
-//////////////sticky menu
-document.addEventListener("DOMContentLoaded", function(){
-		
-    window.addEventListener('scroll', function() {
-       
-        if (window.scrollY > 200) {
-            document.getElementById('navbar_top').classList.add('fixed-top','shadow');
-            // add padding top to show content behind navbar
-            navbar_height = document.querySelector('.navbar').offsetHeight;
-            document.body.style.paddingTop = navbar_height + 'px';
-        } else {
-             document.getElementById('navbar_top').classList.remove('fixed-top','shadow');
-             // remove padding top from body
-            document.body.style.paddingTop = '0';
-        } 
-    });
-}); 
