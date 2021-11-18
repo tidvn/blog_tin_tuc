65be00585ffd58960fe0a0f2ec9f39bcdbb4fb94
ppp103 <74097281+ppp103@users.noreply.github.com>
2021-11-18 22:28:15 +0700

    chỉnh sửa post
    Thêm header

--- assets/js/sticky.js
+++ assets/js/sticky.js
@@ -1,16 +1,16 @@
-document.addEventListener("DOMContentLoaded", function(){
-		
-    window.addEventListener('scroll', function() {
-       
+document.addEventListener("DOMContentLoaded", function () {
+
+    window.addEventListener('scroll', function () {
+
         if (window.scrollY > 200) {
-            document.getElementById('navbar_main').classList.add('fixed-top','shadow');
+            document.getElementById('navbar_main').classList.add('fixed-top', 'shadow');
             // add padding top to show content behind navbar
             navbar_height = document.querySelector('.navbar').offsetHeight;
             document.body.style.paddingTop = navbar_height + 'px';
         } else {
-             document.getElementById('navbar_main').classList.remove('fixed-top','shadow');
-             // remove padding top from body
+            document.getElementById('navbar_main').classList.remove('fixed-top', 'shadow');
+            // remove padding top from body
             document.body.style.paddingTop = '0';
-        } 
+        }
     });
-}); 
+});
